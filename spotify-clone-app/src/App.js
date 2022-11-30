8f997504b41d62b61c72cef215de5febed8c0b15
hshirk <hshirk@uw.edu>
2022-11-29 16:02:36 -0800

    added data layer

--- spotify-clone-app/src/App.js
+++ spotify-clone-app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Login from "./Login";
 import { getTokenFromUrl } from "./spotify";
 import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
 
 const spotify = new SpotifyWebApi();
 
@@ -29,7 +30,7 @@ function App() {
   return (
     <div className="app">
       {
-      token ? (<h1>I am logged in</h1>) : (<Login />)
+      token ? <Player/> : <Login/>
       }
 
     </div>
